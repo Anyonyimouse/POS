7cdb1d3f9a1c3dfb6a1182383fd966322912f291
CARL <CARL@LAPTOP-J04C8II5>
2024-10-02 19:16:36 +0800

    Add project files.

---
+++ POS/Form1.Designer.cs
@@ -0,0 +1,407 @@
+﻿namespace POS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel4 = new Panel();
+            pictureBox3 = new PictureBox();
+            richTextBox1 = new RichTextBox();
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox2 = new GroupBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            panel7 = new Panel();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel7.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 192, 128);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(44, 11);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(315, 235);
+            panel2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(20, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(281, 207);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(255, 224, 192);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(44, 269);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(315, 235);
+            panel3.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(20, 14);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(281, 207);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(255, 192, 192);
+            panel4.Controls.Add(pictureBox3);
+            panel4.Location = new Point(44, 529);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(315, 235);
+            panel4.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(17, 23);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(281, 199);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(983, 11);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(295, 753);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(17, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 207);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Flavors";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(65, 128);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(117, 24);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(65, 80);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(117, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(65, 26);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(117, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox3);
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Controls.Add(checkBox1);
+            groupBox2.Location = new Point(14, 18);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(250, 207);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Add Ons";
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(84, 153);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(101, 24);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "checkBox3";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(84, 102);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(101, 24);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(84, 47);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(24, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(250, 27);
+            textBox1.TabIndex = 8;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.White;
+            label1.Location = new Point(24, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 20);
+            label1.TabIndex = 9;
+            label1.Text = "Amount: 0";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 117);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 10;
+            button1.Text = "Check out";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Location = new Point(165, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 22);
+            label2.TabIndex = 11;
+            label2.Text = "Change: 0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(365, 11);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 38);
+            label3.TabIndex = 12;
+            label3.Text = "Cookies";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.IndianRed;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(panel7);
+            panel1.Controls.Add(panel6);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(richTextBox1);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1290, 783);
+            panel1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(365, 518);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 38);
+            label5.TabIndex = 18;
+            label5.Text = "Pizza";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(365, 269);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 38);
+            label4.TabIndex = 17;
+            label4.Text = "Fries";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.FromArgb(255, 224, 192);
+            panel7.Controls.Add(label1);
+            panel7.Controls.Add(textBox1);
+            panel7.Controls.Add(button1);
+            panel7.Controls.Add(label2);
+            panel7.Location = new Point(637, 529);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(288, 238);
+            panel7.TabIndex = 16;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(255, 224, 192);
+            panel6.Controls.Add(groupBox2);
+            panel6.Location = new Point(637, 269);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(288, 235);
+            panel6.TabIndex = 16;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(255, 224, 192);
+            panel5.Controls.Add(groupBox1);
+            panel5.Location = new Point(637, 11);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(288, 238);
+            panel5.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1290, 777);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Panel panel3;
+        private PictureBox pictureBox2;
+        private Panel panel4;
+        private PictureBox pictureBox3;
+        private RichTextBox richTextBox1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Panel panel1;
+        private Panel panel6;
+        private Panel panel5;
+        private Label label5;
+        private Label label4;
+        private Panel panel7;
+    }
+}
---
+++ POS/Form1.cs
@@ -0,0 +1,236 @@
+﻿namespace POS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int priceV1 = 0;
+        int priceV2 = 0;
+        int priceV3 = 0;
+
+        int priceAdd1 = 0;
+        int priceAdd2 = 0;
+        int priceAdd3 = 0;
+
+        int Total = 0;
+        int amount = 0;
+
+        string prod;
+        string variety;
+        string addOns1;
+        int change, cash;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Total = priceV2;
+            label1.Text = "Amount: " + Total;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Flavors("Chocolate Chip", "Peanut Butter", "Matcha Green Tea");
+            addOns("Walnuts", "coconut flakes", "White chocolate chips");
+            Prices(50, 45, 60, 30, 15, 10);
+           
+            prod = "Cookies";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            label1.Text = "Amount: 0";
+            label2.Text = "Change: 0";
+            textBox1.Clear();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Flavors("Classic French Fries", "Cajun Fries", "Buffalo Fries");
+            addOns("Cheese sauce", "truffle oil", " jalapeños");
+            Prices(60, 70, 80, 15, 30, 10);
+            prod = "Fries";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            label1.Text = "Amount: 0";
+            label2.Text = "Change: 0";
+            textBox1.Clear();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Flavors("Hawain", "Combination (Pepperoni and Ham)", "Triple Chees");
+            addOns("Cheese", "Meat", "Ham");
+            Prices(125, 150, 145, 20, 20, 15);
+            prod = "Pizza";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            label1.Text = "Amount: 0";
+            label2.Text = "Change: 0";
+            textBox1.Clear();
+        }
+
+        void Flavors(string v1, string v2, string v3)
+        {
+            radioButton1.Text = v1;
+            radioButton2.Text = v2;
+            radioButton3.Text = v3;
+
+
+        }
+
+        void addOns(string v1, string v2, string v3)
+        {
+            checkBox1.Text = v1;
+            checkBox2.Text = v2;
+            checkBox3.Text = v3;
+        }
+        void Prices(int p1, int p2, int p3, int a1, int a2, int a3)
+        {
+            priceV1 = p1;
+            priceV2 = p2;
+            priceV3 = p3;
+
+            priceAdd1 = a1;
+            priceAdd2 = a2;
+            priceAdd3 = a3;
+        }
+
+        void prices()
+        {
+            Boolean addons1 = checkBox1.Checked;
+            Boolean addons2 = checkBox2.Checked;
+            Boolean addons3 = checkBox3.Checked;
+            Boolean prod1 = radioButton1.Checked;
+            Boolean prod2 = radioButton2.Checked;
+            Boolean prod3 = radioButton3.Checked;
+
+            int addons = 0;
+            List<string> selectedAddOns = new List<string>();
+            List<string> selectedProd = new List<string>();
+
+            if (addons1)
+            {
+                addons += priceAdd1;
+                selectedAddOns.Add(checkBox1.Text);
+            }
+            if (addons2)
+            {
+                addons += priceAdd2;
+                selectedAddOns.Add(checkBox2.Text);
+            }
+            if (addons3)
+            {
+                addons += priceAdd3;
+                selectedAddOns.Add(checkBox3.Text);
+            }
+
+            if (prod1)
+            {
+                selectedProd.Add(radioButton1.Text);
+            }
+            else if (prod2)
+            {
+                selectedProd.Add(radioButton2.Text);
+            }
+            else if (prod3)
+            {
+                selectedProd.Add(radioButton3.Text);
+            }
+            variety = string.Join(" ", selectedProd);
+            addOns1 = string.Join(", ", selectedAddOns);
+            amount = addons + Total;
+            label1.Text = "Amount: " + amount;
+
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Total = priceV1;
+            label1.Text = "Amount: " + Total;
+
+            variety = "";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Total = priceV3;
+            label1.Text = "Amount: " + Total;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            prices();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            prices();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            prices();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            receipt();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            label1.Text = "Amount: 0";
+            label2.Text = "Change: 0";
+            textBox1.Clear();
+        }
+
+        void receipt()
+        {
+            richTextBox1.Text = richTextBox1.Text 
+                +"------------------------------------------------"
+                + "You ordered: "
+                + prod + "\n"
+                + "Flavor: "
+                + variety + "\n"
+                + "Add Ons: " 
+                + addOns1 + "\n"
+                + "Price:₱ "
+                + amount + "\n" 
+                + "You Pay: "
+                + cash + "\n"
+                + "Total change: "
+                + change + "\n"
+                + "Thank You For Ordering!" + "\n"
+                +"------------------------------------------------";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Equals(""))
+            {
+                cash = Convert.ToInt32(textBox1.Text);
+                change =  cash - amount;
+                label2.Text = "Change:" + change;
+            }
+        }
+    }
+}
